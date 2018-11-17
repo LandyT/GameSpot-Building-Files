038664f8f5f92cb4bec2d05ffba6453718c02390
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-11-17 21:18:11 +0100

    Create Coment.cs

---
+++ gamesport/gamesport/Models/Coment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gamesport.Models
+{
+    public class Coment
+    {
+        public int id { get; set; }
+        public String comentario { get; set; }
+        public String usuario { get; set; }
+        public int fecha { get; set; }
+
+    }
+}

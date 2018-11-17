644b59be228b1a320c1d2010ae25c67db1815c9c
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-11-17 21:18:09 +0100

    Create Anunc.cs

---
+++ gamesport/gamesport/Models/Anunc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gamesport.Models
+{
+    public class Anunc
+    {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public int Foto { get; set; }
+        public String Descripción { get; set; }
+        public int fecha { get; set; }
+        public String Dirección { get; set; }
+        public int Id_usuario { get; set; }
+        public String ListaComentario { get; set; }
+    }
+}

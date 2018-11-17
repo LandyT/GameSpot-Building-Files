95864b825f823a0209a9ff007fa04ed367a57042
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-11-17 21:18:35 +0100

    Create Usuari.cs

---
+++ gamesport/gamesport/Models/Usuari.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gamesport.Models
+{
+    public class Usuari
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public int Teléfono { get; set; }
+        public String Dirección { get; set; }
+        public int Edad { get; set; }
+
+        public String Correo { get; set; }
+
+        public String Contraseña { get; set; }
+    }
+}

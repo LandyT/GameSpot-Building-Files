21dcad7082b6b9e056b2436c6ecfa2833d9c92ab
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-11-17 21:17:41 +0100

    Create comentariosController.cs

---
+++ gamesport/gamesport/Controllers/comentariosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using gamesport.Models;
+
+namespace gamesport.Controllers
+{
+    public class comentariosController : Controller
+    {
+        private gamesportEntities db = new gamesportEntities();
+
+        // GET: comentarios
+        public ActionResult Index()
+        {
+            return View(db.comentarios.ToList());
+        }
+
+        // GET: comentarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentarios.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // GET: comentarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: comentarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,comentario1,usuario,fecha")] comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.comentarios.Add(comentario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comentario);
+        }
+
+        // GET: comentarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentarios.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // POST: comentarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,comentario1,usuario,fecha")] comentario comentario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comentario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comentario);
+        }
+
+        // GET: comentarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comentario comentario = db.comentarios.Find(id);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentario);
+        }
+
+        // POST: comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            comentario comentario = db.comentarios.Find(id);
+            db.comentarios.Remove(comentario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

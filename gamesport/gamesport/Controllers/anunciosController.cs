5b22d8355449a59b1054b4afa34866cad5f9531d
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-11-17 21:17:45 +0100

    Create anunciosController.cs

---
+++ gamesport/gamesport/Controllers/anunciosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using gamesport.Models;
+
+namespace gamesport.Controllers
+{
+    public class anunciosController : Controller
+    {
+        private gamesportEntities db = new gamesportEntities();
+
+        // GET: anuncios
+        public ActionResult Index()
+        {
+            return View(db.anuncios.ToList());
+        }
+
+        // GET: anuncios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            anuncio anuncio = db.anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // GET: anuncios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: anuncios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Foto,Descripción,fecha,Dirección,Id_usuario,listaComentario")] anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.anuncios.Add(anuncio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(anuncio);
+        }
+
+        // GET: anuncios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            anuncio anuncio = db.anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: anuncios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Foto,Descripción,fecha,Dirección,Id_usuario,listaComentario")] anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(anuncio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(anuncio);
+        }
+
+        // GET: anuncios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            anuncio anuncio = db.anuncios.Find(id);
+            if (anuncio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(anuncio);
+        }
+
+        // POST: anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            anuncio anuncio = db.anuncios.Find(id);
+            db.anuncios.Remove(anuncio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

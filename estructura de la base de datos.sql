830bc1c63f3a27bb264ea365de45fda1f95041d0
Jose Alberto Galva Mateo <jgm2403@gmail.com>
2018-12-02 19:34:10 +0100

    Update estructura de la base de datos.sql

--- estructura de la base de datos.sql
+++ estructura de la base de datos.sql
@@ -2,14 +2,14 @@ create database gamesport
 use gamesport
 
 create table usuario(
-Id	int IDENTITY(1,1),
-Nombre	Varchar(40),
-Apellido	Varchar(40),
-Teléfono	int,
-Dirección	Varchar(40),
-Edad	int,
-Correo	varchar(40),
-Contraseña 	varchar(40),
+Id	int IDENTITY(1,1) not null,
+Nombre	Varchar(40) not null,
+Apellido	Varchar(40)not null,
+Teléfono	int not null,
+Dirección	Varchar(40)not null,
+Edad	int not null,
+Correo	varchar(40) not null,
+Contraseña 	varchar(40) not null,
 constraint pkid primary key (id)
 );
 
@@ -17,21 +17,33 @@ select * from usuario;
 select * from anuncio;
 
 create table anuncio (
-Id	int  IDENTITY(1,1),
-Nombre	Varchar(40),
-Foto	int,
-Descripción 	Varchar(40),
-fecha date,
-Dirección	Varchar(40),
-Id_usuario	int,
-listaComentario	Varchar(40),
-constraint pk primary key (id)
-);
+Id	int  IDENTITY(1,1) not null,
+Nombre	Varchar(40) not null,
+Foto	varchar not null,
+Descripción 	Varchar(40) not null,
+fecha date not null,
+Dirección	Varchar(40) not null,
+Id_usuario	int not null,
+id_categoria int,
+listaComentario	Varchar(40) not null,
+constraint pk primary key (id),
+)
+
+ALTER TABLE anuncio 
+   ADD CONSTRAINT fk
+   FOREIGN KEY (id_categoria) 
+   REFERENCES categoria(id);
 
 create table comentario(
-Id	int  IDENTITY(1,1),
-comentario varchar (250),
-usuario varchar(40),
-fecha date
+Id	int  IDENTITY(1,1) not null,
+comentario varchar (250) not null,
+usuario varchar(40) not null,
+fecha date not null,
 constraint pkd primary key (id)
+)
+
+create table categoria(
+id	int  IDENTITY(1,1) not null,
+nombre varchar(20) not null,
+constraint pkp primary key (id)
 )
